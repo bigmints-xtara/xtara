ad2fafc7f3b0582a93c34f8762c3ea58b2bb4c7a
Pretheesh Thomas <me@bigmints.com>
2026-06-16 08:49:37 +0400

    Apply session security-hardcoded-firebase-config (9266897755270929373)

--- src/lib/algolia/algolia.ts
+++ src/lib/algolia/algolia.ts
@@ -1,8 +1,8 @@
 import { liteClient as algoliasearch } from "algoliasearch/lite";
 
-const APP_ID = "94C231P5QA";
-const SEARCH_API_KEY = "7d87dea285e1b098041840f354ce9e8e";
-const COLLEGES_INDEX_NAME = "xtara_colleges";
+const APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || "DEV_APP_ID";
+const SEARCH_API_KEY = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || "DEV_SEARCH_KEY";
+const COLLEGES_INDEX_NAME = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || "xtara_colleges";
 
 export const algoliaClient = algoliasearch(APP_ID, SEARCH_API_KEY);
 
